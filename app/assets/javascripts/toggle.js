e41db5536474ec16f489931605ebff485764a094
Tim Carlock <timothy.carlock@gmail.com>
2013-01-19 15:15:43 -0800

    refactored markup and CSS for feed and expand collapse

--- app/assets/javascripts/toggle.js
+++ app/assets/javascripts/toggle.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
-  $('.hide').click(function(){
-    $(this).hide();
-    $('#showhide_'+$(this).attr('resource')).hide();
-    $('#show_'+$(this).attr('resource')).show();
+  $('.events .toggle').click(function() {
+    $(this).toggleClass('expanded');
+    $(this).siblings('.showhide').slideToggle(300);
+    $(this).find('i').toggleClass('icon-angle-down').toggleClass('icon-angle-up')
+
+    if ($(this).hasClass('expanded'))
+      $(this).find('span').text('Collapse')
+    else
+      $(this).find('span').text('Expand')
   });
-  $('.show').click(function(){
-    $(this).hide();
-    $('#showhide_'+$(this).attr('resource')).show();
-    $('#hide_'+$(this).attr('resource')).show();
-  });
+
+  // $('.hide').click(function(){
+  //   $(this).hide();
+  //   $('#showhide_'+$(this).attr('resource')).hide();
+  //   $('#show_'+$(this).attr('resource')).show();
+  // });
+  // $('.show').click(function(){
+  //   $(this).hide();
+  //   $('#showhide_'+$(this).attr('resource')).show();
+  //   $('#hide_'+$(this).attr('resource')).show();
+  // });
 });
