4dae1dffbc04f073d868043e4226001299bf4519
Tim Carlock <timothy.carlock@gmail.com>
2013-01-19 20:40:19 -0800

    Markup and css cleanup for activity list

--- app/assets/javascripts/toggle.js
+++ app/assets/javascripts/toggle.js
@@ -3,21 +3,5 @@ $(document).ready(function() {
     $(this).toggleClass('expanded');
     $(this).siblings('.showhide').slideToggle(300);
     $(this).find('i').toggleClass('icon-angle-down').toggleClass('icon-angle-up')
-
-    if ($(this).hasClass('expanded'))
-      $(this).find('span').text('Collapse')
-    else
-      $(this).find('span').text('Expand')
   });
-
-  // $('.hide').click(function(){
-  //   $(this).hide();
-  //   $('#showhide_'+$(this).attr('resource')).hide();
-  //   $('#show_'+$(this).attr('resource')).show();
-  // });
-  // $('.show').click(function(){
-  //   $(this).hide();
-  //   $('#showhide_'+$(this).attr('resource')).show();
-  //   $('#hide_'+$(this).attr('resource')).show();
-  // });
 });
